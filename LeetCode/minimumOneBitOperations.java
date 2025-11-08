ee2f758b0b6c8b13b3980cb6dfb2c1750e279f48
Phạm Hoàng Nhân <93658293+q1xuanx@users.noreply.github.com>
2025-11-08 13:53:35 +0700

    Add minimumOneBitOperations method in Solution class

---
+++ LeetCode/minimumOneBitOperations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n > 0) {
+            res ^= n; 
+            n >>= 1;
+        }
+        return res;
+    }
+}
+
+// Gray codes 
