d60703e40325cb4d90e43a555f9587011f7ec20f
nhan <nhoang2929@gmail.com>
2026-05-08 22:40:54 +0700

    fix

--- Kien_Thuc_Co_Ban/Ma_Tran_Vuong.java
+++ Kien_Thuc_Co_Ban/Ma_Tran_Vuong.java
@@ -41,9 +41,9 @@ public class Ma_Tran_Vuong {
 	public static void xuatMang(int a[][], int n) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				System.out.print(a[i][j] + "\t");
+				System.out.print(a[i][j] + " ");
 			}
-			System.out.println("\n");
+			System.out.println();
 		}
 	}
 
